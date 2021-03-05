691797d567cc6e67bbe88c0c51ccf291e82622d3
Billy Lin <billylin80125@gmail.com>
2021-03-04 18:24:53 -0800

    tested 2 ways of changing motor directions

--- test/test.ino
+++ test/test.ino
@@ -6,6 +6,24 @@ int motorDir = 1;
 bool decrement_flag = false;
 int motorSpeed = 0;
 
+void setup() {
+  //motor initialization
+  pinMode(motor1_enA, OUTPUT);
+  pinMode(motor1_in1, OUTPUT);
+  pinMode(motor1_in2, OUTPUT);
+  digitalWrite(motor1_in1, motorDir);
+  digitalWrite(motor1_in2, !motorDir);
+  digitalWrite(13, motorDir);
+  //button initialization
+  pinMode(2, INPUT); //button
+  attachInterrupt(digitalPinToInterrupt(2), button_ISR, CHANGE); //give an ISR to button
+  //LED
+  pinMode(13, OUTPUT); //LED for showing button press
+  //debugging serial port
+  Serial.begin(9600);
+}
+
+/* method 1 for changing direction: Ramp down to 0, turn, and ramp back up.*/
 void button_ISR() {
   //debouncing
   static unsigned long last_time = 0;
@@ -15,21 +33,6 @@ void button_ISR() {
     decrement_flag = true;
   }
   last_time = interrupt_time;
-  /*decrement speed to 0 and increment back*/  
-}
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(motor1_enA, OUTPUT);
-  pinMode(motor1_in1, OUTPUT);
-  pinMode(motor1_in2, OUTPUT);
-  Serial.begin(9600);
-  pinMode(2, INPUT); //button
-  attachInterrupt(digitalPinToInterrupt(2), button_ISR, CHANGE); //give an ISR to button
-  pinMode(13, OUTPUT); //LED for showing button press
-  digitalWrite(motor1_in1, motorDir);
-  digitalWrite(motor1_in2, !motorDir);
-  digitalWrite(13, motorDir);
 }
 
 void loop() {
@@ -37,6 +40,7 @@ void loop() {
   int motorSpeed_next = map(pot_val, 0, 1023, 0, 255);
   double diff = motorSpeed_next - motorSpeed;
   if (decrement_flag && motorSpeed >= 1) {
+    Serial.println("decrement!");
     motorSpeed -= 1;
     if (motorSpeed == 0){
       decrement_flag = false;
@@ -49,9 +53,45 @@ void loop() {
     motorSpeed += diff > 0 ? 1 : -1;
     analogWrite(motor1_enA, motorSpeed);
   }
-  Serial.print("new data ");
-  Serial.println(motorSpeed_next);
-  Serial.print("current data ");
-  Serial.println(motorSpeed);
+//  Serial.print("new data ");
+//  Serial.println(motorSpeed_next);
+//  Serial.print("current data ");
+//  Serial.println(motorSpeed);
   delay(1);
 }
+
+/*method 2 for changing direction: brake for n seconds and change direction without ramping down speed*/
+/*
+void button_ISR() {
+  //debouncing
+  static unsigned long last_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time-last_time > 200) {
+    motorDir = !motorDir;
+    //brake for 0.5s then reverse
+    digitalWrite(motor1_in1, LOW);
+    digitalWrite(motor1_in2, LOW);
+    delay(1000); //delay for 1 sec
+    Serial.println("brake!");
+    digitalWrite(motor1_in1, motorDir);
+    digitalWrite(motor1_in2, !motorDir);
+    digitalWrite(13, motorDir);
+  }
+  last_time = interrupt_time;
+}
+
+void loop() {
+  int pot_val = analogRead(A0); //read from potentiometer 0~1023
+  int motorSpeed_next = map(pot_val, 0, 1023, 0, 255);
+  double diff = motorSpeed_next - motorSpeed;
+  if (motorSpeed != motorSpeed_next){
+    motorSpeed += diff > 0 ? 1 : -1;
+    analogWrite(motor1_enA, motorSpeed);
+  }
+//  Serial.print("new data ");
+//  Serial.println(motorSpeed_next);
+//  Serial.print("current data ");
+//  Serial.println(motorSpeed);
+  delay(1);
+}
+*/
