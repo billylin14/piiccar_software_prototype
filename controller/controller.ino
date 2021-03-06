bf07ba2ed36c942468ff6eba262631632238e341
Billy Lin <billylin80125@gmail.com>
2021-03-05 23:38:44 -0800

    tested the joystick and it outputs correctly

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,28 +1,35 @@
-#define input_x  //analog pin for x coordinate
-#define input_y //analog pin for y coordinate
-#define input_en //digital pin for enabling the joystick 
+#define VRx A1 //analog pin A1 for x coordinate
+#define VRy A0 //analog pin A0 for y coordinate
+#define VRsw 2 //digital pin D2 for z input (high/low) 
 
-#define bt_rx 0
-#define bt_tx 1
-
-#include <SoftwareSerial.h>
-SoftwareSerial BT(bt_rx, bt_tx);
+//#define bt_rx 0
+//#define bt_tx 1
+//
+//#include <SoftwareSerial.h>
+//SoftwareSerial BT(bt_rx, bt_tx);
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(input_en, INPUT);
-  digitalWrite(input_en, HIGH); //enable the joystick
-
-  BT.begin(9600); //setting up baud rate
+  pinMode(VRx, INPUT);
+  pinMode(VRy, INPUT);
+  pinMode(VRsw, INPUT_PULLUP); //1 as unpressed, 0 as pressed
+  Serial.begin(9600);
+  
+//  BT.begin(9600); //setting up baud rate
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   /* reading joystick values */
-  double x_val = analogRead(input_x);
-  double y_val = analogRead(input_y);
-  /* sending values to uno */
-  Bt.write(); //versus BT.print()
-  delay(50); //arbitrary delay, need to adjust later
+  int x_val = analogRead(VRx);
+  int y_val = analogRead(VRy);
+  int z_val = digitalRead(VRsw);
+//  digitalWrite(VRsw, HIGH);
+  Serial.println(x_val);
+  Serial.println(y_val);
+  Serial.println(z_val);
+  Serial.println();
+  delay(500);
+//  /* sending values to uno */
+//  Bt.write(); //versus BT.print()
+//  delay(50); //arbitrary delay, need to adjust later
   
 }
